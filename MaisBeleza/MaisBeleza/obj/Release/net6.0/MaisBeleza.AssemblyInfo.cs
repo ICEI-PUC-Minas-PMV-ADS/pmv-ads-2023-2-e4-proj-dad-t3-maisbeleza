fbc956dc87fb16d55140d2996f9ce78b2b963de5
Juliana Calazans <julianacalazansmr@gmail.com>
2023-09-17 17:31:23 -0300

    Criação da Classe e Controller de  Serviços.

--- MaisBeleza/MaisBeleza/obj/Release/net6.0/MaisBeleza.AssemblyInfo.cs
+++ MaisBeleza/MaisBeleza/obj/Release/net6.0/MaisBeleza.AssemblyInfo.cs
@@ -11,7 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MaisBeleza")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("MaisBeleza")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MaisBeleza")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
