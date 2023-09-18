1ee4b51583565264df03d6e80b3e840f867b674c
Rúbia Karina <rubiakarina22@gmail.com>
2023-09-18 17:41:18 -0300

    Model e Controller

---
+++ MaisBeleza/MaisBeleza/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaisBeleza.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public int Telefone { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public ICollection<Agendamento> Agendamentos { get; set;}
+
+    }
+
+
+}
