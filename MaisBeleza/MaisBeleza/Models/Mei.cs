82956107cb8f3f21245e36c1649010513dca19cb
Guilherme Faustini <gfaustini@gmail.com>
2023-09-19 15:01:57 -0300

    Mei (model, controller)

---
+++ MaisBeleza/MaisBeleza/Models/Mei.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaisBeleza.Models
+{
+    [Table("Meis")]
+    public class Mei : LinksHATEOS
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NomeMei { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Required]
+        public string Rua { get; set; }
+        [Required]
+        public int Numero { get; set; }
+        [Required]
+        public string Bairro { get; set; }
+        [Required]
+        public string Cidade { get; set; }
+        [Required]
+        public string Estado { get; set; }
+        [Required]
+        public string Senha { get; set; }
+    }
+}
