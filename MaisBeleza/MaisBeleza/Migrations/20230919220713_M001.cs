2c0f94dee00677cba06eb8cc850823342897bed2
Daiane Procópio <1244910@sga.pucminas.br>
2023-09-19 19:07:53 -0300

    Banco de dados

---
+++ MaisBeleza/MaisBeleza/Migrations/20230919220713_M001.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaisBeleza.Migrations
+{
+    /// <inheritdoc />
+    public partial class M001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeMei = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rua = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HorarioFuncionamento = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Horario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MeiId = table.Column<int>(type: "int", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Meis_MeiId",
+                        column: x => x.MeiId,
+                        principalTable: "Meis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faturamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MeiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faturamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faturamentos_Meis_MeiId",
+                        column: x => x.MeiId,
+                        principalTable: "Meis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeServico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duracao = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    MeiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Servicos_Meis_MeiId",
+                        column: x => x.MeiId,
+                        principalTable: "Meis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agenda",
+                columns: table => new
+                {
+                    AgendamentoId = table.Column<int>(type: "int", nullable: false),
+                    ServicoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agenda", x => new { x.AgendamentoId, x.ServicoId });
+                    table.ForeignKey(
+                        name: "FK_Agenda_Agendamentos_AgendamentoId",
+                        column: x => x.AgendamentoId,
+                        principalTable: "Agendamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Agenda_Servicos_ServicoId",
+                        column: x => x.ServicoId,
+                        principalTable: "Servicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LinkDto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Href = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Metodo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AgendamentoId = table.Column<int>(type: "int", nullable: true),
+                    FaturamentoId = table.Column<int>(type: "int", nullable: true),
+                    MeiId = table.Column<int>(type: "int", nullable: true),
+                    ServicoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LinkDto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Agendamentos_AgendamentoId",
+                        column: x => x.AgendamentoId,
+                        principalTable: "Agendamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Faturamentos_FaturamentoId",
+                        column: x => x.FaturamentoId,
+                        principalTable: "Faturamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Meis_MeiId",
+                        column: x => x.MeiId,
+                        principalTable: "Meis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Servicos_ServicoId",
+                        column: x => x.ServicoId,
+                        principalTable: "Servicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agenda_ServicoId",
+                table: "Agenda",
+                column: "ServicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_ClienteId",
+                table: "Agendamentos",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_MeiId",
+                table: "Agendamentos",
+                column: "MeiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faturamentos_MeiId",
+                table: "Faturamentos",
+                column: "MeiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_AgendamentoId",
+                table: "LinkDto",
+                column: "AgendamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_FaturamentoId",
+                table: "LinkDto",
+                column: "FaturamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_MeiId",
+                table: "LinkDto",
+                column: "MeiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_ServicoId",
+                table: "LinkDto",
+                column: "ServicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servicos_MeiId",
+                table: "Servicos",
+                column: "MeiId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agenda");
+
+            migrationBuilder.DropTable(
+                name: "LinkDto");
+
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Faturamentos");
+
+            migrationBuilder.DropTable(
+                name: "Servicos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Meis");
+        }
+    }
+}
