dd4c364a0821b8e112ee565a67745de45f5c6024
Daiane Procópio <1244910@sga.pucminas.br>
2023-09-27 09:50:17 -0300

    Authorize

--- MaisBeleza/MaisBeleza/Controllers/ClientesController.cs
+++ MaisBeleza/MaisBeleza/Controllers/ClientesController.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace MaisBeleza.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ClientesController : ControllerBase
