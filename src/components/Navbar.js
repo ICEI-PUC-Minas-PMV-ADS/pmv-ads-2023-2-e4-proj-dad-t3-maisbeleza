480e473b6cc818685668b449bc08e0a462d015d7
Rúbia Karina <rubiakarina22@gmail.com>
2023-10-28 11:32:04 -0300

    correção do menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import Logo from '../assets/img/Logo.png';
 import { Link } from 'react-router-dom';
-import  { useState, useEffect } from 'react';
+
 
 function Menu() {
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    // Verificar se o usuário está logado quando o componente é montado
-    const authToken = localStorage.getItem('authToken');
-    if (authToken) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, []);
-
   const handleLogout = () => {
     // Limpar o token de autenticação do localStorage
     localStorage.removeItem('authToken');
     // Redirecionar o usuário para a página de login
     window.location.href = '/login';
-    setIsLoggedIn(false);
   };
 
 
@@ -43,20 +29,11 @@ function Menu() {
             <Nav.Link as={Link} to="/servicos" className="text-white">Serviços</Nav.Link>
             <Nav.Link as={Link} to="/faturamento" className="text-white">Faturamento</Nav.Link>
             <Nav.Link as={Link} to="/perfil" className="text-white">Perfil</Nav.Link>
-
-            {/* Condicional para exibir ou ocultar as opções de cadastro e login */}
-            {isLoggedIn ? (
-              <>
-                <Nav.Link as={Link} to="/" className="text-white" onClick={handleLogout}>
-                  Sair
-                </Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/cadastro" className="text-white">Cadastro</Nav.Link>
-                <Nav.Link as={Link} to="/login" className="text-white">Login</Nav.Link>
-              </>
-            )}
+            <Nav.Link as={Link} to="/cadastro" className="text-white">Cadastro</Nav.Link>
+            <Nav.Link as={Link} to="/login" className="text-white">Login</Nav.Link>
+            <Nav.Link as={Link} to="/" className="text-white" onClick={handleLogout}>
+              Sair
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
